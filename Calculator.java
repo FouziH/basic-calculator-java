3706a8ec8d1a4a31e3e6363d20c70df8dc107379
Hamza Mohamed <Hamzafowzi1@gmail.com>
2022-01-14 15:35:45 -0600

    ....

--- Calculator.java
+++ Calculator.java
@@ -41,26 +41,34 @@ public class Calculator {
         return sum;
     }
 
-    // Main method 
+    // Main method
     public static void main(String[] args) {
-        //Creating object myCalculator 
+        // Creating object myCalculator
         Calculator myCalculator = new Calculator();
 
-        //declaring addValue int and assigning it to the result of add method 
+        // declaring addValue int and assigning it to the result of add method
         int addValue = myCalculator.add(5, 7);
 
-        //printing the addValue to the console
+        // printing the addValue to the console
         System.out.println(addValue);
 
-        //declaring subtractValue and assigning it to the value of subtract method 
+        // declaring subtractValue and assigning it to the value of subtract method
+
         int subtractValue = myCalculator.subtract(45, 11);
         System.out.println(subtractValue);
 
+        // int multiple Value hold results of multiply method
         int multipleValue = myCalculator.multiply(45, 11);
+
+        // printing the result to the console
+
         System.out.println(multipleValue);
 
+        // int divisionValue hold the result of divide method 
         int divisionValue = myCalculator.divide(9, 3);
+        //Prints the results to the console
         System.out.println(divisionValue);
+
 
         int moduloValue = myCalculator.modulo(9, 2);
 
